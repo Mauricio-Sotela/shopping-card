5a836ab3d63a946681d89a6f5f96cf4144230a7c
sotela88 <j-ms-27@hotmail.com>
2020-09-17 10:45:20 +0200

    Updates

---
+++ precache-manifest.371e7e403601e8c32f07e7aa18f145bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596a7b9f8ef74670868ad61005e6b12b",
+    "url": "/shopping_car/index.html"
+  },
+  {
+    "revision": "0041b60538c23847264f",
+    "url": "/shopping_car/static/css/main.8cc3bc3f.chunk.css"
+  },
+  {
+    "revision": "600f3bf880b078191c2d",
+    "url": "/shopping_car/static/js/2.b60f4030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping_car/static/js/2.b60f4030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0041b60538c23847264f",
+    "url": "/shopping_car/static/js/main.eb5785b7.chunk.js"
+  },
+  {
+    "revision": "213e3535a2689ad713f2",
+    "url": "/shopping_car/static/js/runtime-main.4a57f05f.js"
+  }
+]);
